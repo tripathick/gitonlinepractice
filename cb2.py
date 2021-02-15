960f143f245490a3514f298af1443afe7bd4da6b
CKT <chandankumar765414@gmail.com>
2021-02-15 16:42:48 +0530

    adding another file

---
+++ cb2.py
@@ -0,0 +1,89 @@
+'''a=1101
+if a>0:
+  print("positive")
+  if a%5==0:
+    print("divisible by 5")
+  else:
+    print("Not divisible") \
+'''
+
+'''
+x=str(input())
+if x<0:
+  sign=-1
+elif x==0:
+  sign=0
+else:
+  sign=1     
+
+'''
+'''
+a=10
+while a>0:
+  print(a)
+  a-=1 
+
+'''
+
+'''
+for i in "python":
+  print(i)
+
+for i in range(2,10):
+  print(i)
+
+for i in range(1,11,2):
+  print(i)  
+
+'''
+'''
+'10+10
+10-5
+10*5
+10/5
+10//3
+10**3
+
+a=10
+a=a+10
+print(a)
+
+a-=10  #a=a-10
+print(a)
+'''
+'''
+#sum and average
+a=200
+b=10
+
+c=a+b
+print(c)
+
+num1=input("Enter 1st number: ")
+num2=input("\n Enter 2nd number: ")
+
+sum=float(num1)+float(num2)
+
+avg=sum/2
+print(avg)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
